1a956a8f4f530530ac6f9fb52390ff27c9141cec
Aaron Hauth <aaronhauth@gmail.com>
2022-06-03 18:06:00 -0500

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+import {unified} from 'unified'
+import retextEnglish from 'retext-english'
+import retextStringify from 'retext-stringify'
+import retextPos from 'retext-pos';
+import ussyfy from './ussify.js';
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const ussyBotMessageFrequency = Number(process.env.ussyBotMessageFrequency);
+const ussifiedWordFrequency = Number(process.env.ussifiedWordFrequency);
+
+// chatbot options
+const opts = {
+  options: {debug: true, messagesLogLevel: 'info'},
+  connection: {
+      reconnect: true,
+      secure: true
+  },
+  identity: {
+      username: process.env.botName,
+      password: 'oauth:' + process.env.chatBotToken
+  },
+  channels: [
+      process.env.channelUserName
+  ]
+}
+
+const chatClient = new tmi.client(opts);
+
+chatClient.connect().catch(console.error);
+
+const processor = unified()
+  .use(retextEnglish)
+  .use(retextPos)
+  .use(ussyfy, {frequency: ussifiedWordFrequency})
+  .use(retextStringify);
+
+  // this chat client really only works in the context of a single channel (mine, at the moment)
+// we initialize our chatTarget so that our redemption bot can have a channel to send our messages to. Otherwise, we don't care too much about this thing here.
+chatClient.on('message', (target, tags, msg, self) => {
+  if (self) return;
+
+  // if we hit the odds of ussyfying a word:
+  if (Math.floor(Math.random()*ussyBotMessageFrequency) === 0) {
+
+      // use the processing stream to ussify the message.
+      const processResult = processor.processSync(msg);
+      chatClient.say(target, processResult.value);
+  } 
+});
---
+++ ussify.js
@@ -0,0 +1,33 @@
+import {visit} from 'unist-util-visit'
+
+const syllableRegex = /[^aeiouy]*[aeiouy]+(?:[^aeiouy]*$|[^aeiouy](?=[^aeiouy]))?/gi;
+
+export default function ussyfy(options = {}) {
+    const frequency = options?.frequency;
+
+    console.warn('frequency not set');
+
+    return (tree, file) => {
+        visit(tree, 'WordNode', node => {
+            const tag = node.data.partOfSpeech;
+            const word = node.children[0].value;
+
+            if (tag[0] !== 'N') return;
+            if (Math.floor(Math.random()*frequency) === 0) return;
+
+            const syllables = word.match(syllableRegex);
+            if (!syllables) return word;
+            var ussyForm = tag[tag.length - 1] === 'S' ? 'ussies' : 'ussy';
+
+            if (word[word.length-1].match(/[^a-zA-Z]/)) {
+                ussyForm += word[word.length-1];
+            }
+
+            console.log(syllables);
+            syllables[syllables.length - 1] = syllables[syllables.length - 1][0] + ussyForm;
+            console.log(word);
+
+            node.children[0].value = syllables.join('');
+        });
+    }
+}
